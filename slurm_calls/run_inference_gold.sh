968a6f867e77aa6853f90072a77fe98f314b1ebb
Uri Katz <uri.katz@aquant.io>
2021-08-21 16:30:16 +0300

    native training loop

--- slurm_calls/run_inference_gold.sh
+++ slurm_calls/run_inference_gold.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export TRANSFORMERS_CACHE=/home/joberant/home/urikatz1/hf_cache
-home/joberant/home/urikatz1/anaconda3/bin/python dense_retriever.py \
+/home/joberant/home/urikatz1/anaconda3/bin/python dense_retriever.py \
  model_file="/home/joberant/home/urikatz1/dpr/DPR/outputs/2021-08-09/23-15-58/output/dpr_biencoder.28"\
  qa_dataset=strategy_gold_dev \
  ctx_datatsets=[dpr_strategyqa_tsv] \
