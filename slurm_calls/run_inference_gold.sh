6edb6782d70814b6cc71324ea66a20adfb242dcb
Uri Katz <uri.katz@aquant.io>
2021-08-27 16:37:48 +0300

    native training loop

--- slurm_calls/run_inference_gold.sh
+++ slurm_calls/run_inference_gold.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export TRANSFORMERS_CACHE=/home/joberant/home/urikatz1/hf_cache
 /home/joberant/home/urikatz1/anaconda3/bin/python /home/joberant/home/urikatz1/dpr/DPR/dense_retriever.py \
- model_file="/home/joberant/home/urikatz1/dpr/DPR/outputs/2021-08-09/23-15-58/output/dpr_biencoder.28"\
+ model_file="/home/joberant/home/urikatz1/dpr/DPR/outputs/2021-08-09/23-15-58/output/dpr_biencoder.15"\
  qa_dataset=strategy_gold_dev \
  ctx_datatsets=[dpr_strategyqa_tsv] \
- encoded_ctx_files=["/home/joberant/home/urikatz1/dpr/datasets/strategyqa/embedded_corpus/dpr_ctx_*"] \
- out_file="/home/joberant/home/urikatz1/dpr/experiments/gold/output/result.json"
+ encoded_ctx_files=["/home/joberant/home/urikatz1/dpr/datasets/strategyqa/embedded_corpus_gold_dpr_biencoder.15/dpr_ctx_*"] \
+ out_file="/home/joberant/home/urikatz1/dpr/experiments/gold/gold_extra_content_dpr_biencoder.15/ir_result/retriever_result.json"
+
+
