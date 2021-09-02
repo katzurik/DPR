68340f9b09cf02ca872f81a706344e63f3140f8d
Uri Katz <uri.katz@aquant.io>
2021-09-02 21:12:31 +0300

    native training loop

--- slurm_calls/dpr_ret_train_2_9_2021.sh
+++ slurm_calls/dpr_ret_train_2_9_2021.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export TRANSFORMERS_CACHE=/home/joberant/home/urikatz1/hf_cache
-/home/joberant/home/urikatz1/anaconda3/bin/python -m torch.distributed.launch --nproc_per_node=4  /home/joberant/home/urikatz1/dpr/DPR/train_dense_encoder.py train_datasets=[strategy_gold_train] dev_datasets=[strategy_gold_dev] output_dir='outputs'
+/home/joberant/home/urikatz1/anaconda3/bin/python -m torch.distributed.launch --nproc_per_node=4  /home/joberant/home/urikatz1/dpr/DPR/train_dense_encoder.py train_datasets=[strategy_gold_train] dev_datasets=[strategy_gold_dev] output_dir='/home/joberant/home/urikatz1/dpr/DPR/outputs/'
