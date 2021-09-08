eccc1d8d44eee1f5dfb8c2039442b4fe92f44401
Uri Katz <uri.katz@aquant.io>
2021-09-08 14:57:25 +0300

    native training loop

--- slurm_calls/run_inference_extracontent_gold.sh
+++ slurm_calls/run_inference_extracontent_gold.sh
@@ -3,7 +3,7 @@ export TRANSFORMERS_CACHE=/home/joberant/home/urikatz1/hf_cache
 /home/joberant/home/urikatz1/anaconda3/bin/python /home/joberant/home/urikatz1/dpr/DPR/dense_retriever.py \
  model_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g/training_output_bsz128/dpr_biencoder.28"\
  hydra.run.dir="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g/inference/"\
- qa_dataset=strategy_extra_content_gold_dev \
+ qa_dataset=strategy_gold_dev \
  ctx_datatsets=[dpr_strategyqa_tsv] \
  encoded_ctx_files=["/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g/dense_embeddings/dpr_ctx_*"] \
- out_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g/inference/retriever_result_extracontent_gold.json"
+ out_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g/inference/retriever_result.json"
