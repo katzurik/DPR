9faebd9a254bce6bf6c57daa826c879160b46ef2
Uri Katz <uri.katz@aquant.io>
2021-09-11 13:09:21 +0300

    native training loop

--- conf/datasets/StrategyQA dataset/modified_data/create_modified_data_for_dpr.py
+++ conf/datasets/StrategyQA dataset/modified_data/create_modified_data_for_dpr.py
@@ -1,39 +1,40 @@
 import json
 
 datatype = 'dev'
-path = '../strategyQA_' + datatype + '.json'
+#path = '../strategyQA_' + datatype + '.json'
+path = '/Users/uri/Documents/Uri/school/Thesis/Implicit/thesis/DPR/conf/datasets/StrategyQA dataset/gold/strategyQA_dev.json'
 with open(path) as json_file:
     data = json.load(json_file)
 
 
-# path = '/Users/uri/Documents/Uri/school/Thesis/Implicit/thesis/Experiments/Extract_strategy_from_SQA_decomp/Strategy_extraction/seq2seq_strategy_extractor/data/strategyqa_data_with_labels/' + datatype + '.json'
-# with open(path) as json_file:
-#     labeled_data = json.load(json_file)
-#
-# for i,sample_in_dpr_data in enumerate(data):
-#     question_changed = False
-#     question = sample_in_dpr_data['question']
-#     for sample_in_labeld_data in labeled_data:
-#         if sample_in_labeld_data['question'] == question:
-#             extra_content = sample_in_labeld_data['extra_content_outer']
-#             sample_in_dpr_data['question'] = '{} {}'.format(question,extra_content)
-#             question_changed = True
-#             break
-#
-#     if not question_changed:
-#         print('i')
-#
-#
-# output_file = 'gold/strategyQA_'+datatype+'.json'
-# with open(output_file,"w") as fd:
-#         json.dump(data, fd)
-
-
-#####
 path = '/Users/uri/Documents/Uri/school/Thesis/Implicit/thesis/Experiments/Extract_strategy_from_SQA_decomp/Strategy_extraction/seq2seq_strategy_extractor/data/strategyqa_data_with_labels/' + datatype + '.json'
 with open(path) as json_file:
     labeled_data = json.load(json_file)
 
+for i,sample_in_dpr_data in enumerate(data):
+    question_changed = False
+    question = sample_in_dpr_data['question']
+    for sample_in_labeld_data in labeled_data:
+        if sample_in_labeld_data['question'] == question:
+            extra_content = sample_in_labeld_data['extra_content_outer']
+            sample_in_dpr_data['question'] = '{} {}'.format(question,extra_content)
+            question_changed = True
+            break
+
+    if not question_changed:
+        print('i')
+
+
+output_file = 'gold/strategyQA_'+datatype+'.json'
+with open(output_file,"w") as fd:
+        json.dump(data, fd)
+
+
+#####
+# path = '/Users/uri/Documents/Uri/school/Thesis/Implicit/thesis/Experiments/Extract_strategy_from_SQA_decomp/Strategy_extraction/seq2seq_strategy_extractor/data/strategyqa_data_with_labels/' + datatype + '.json'
+# with open(path) as json_file:
+#     labeled_data = json.load(json_file)
+#
 # for i,sample_in_dpr_data in enumerate(data):
 #     question_changed = False
 #     question = sample_in_dpr_data['question']
