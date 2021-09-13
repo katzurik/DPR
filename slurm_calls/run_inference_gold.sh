c50b1538906d95d4b636752d8f1fad08d564deba
Uri Katz <uri.katz@aquant.io>
2021-09-13 06:40:46 +0300

    native training loop

--- slurm_calls/run_inference_gold.sh
+++ slurm_calls/run_inference_gold.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export TRANSFORMERS_CACHE=/home/joberant/home/urikatz1/hf_cache
 /home/joberant/home/urikatz1/anaconda3/bin/python /home/joberant/home/urikatz1/dpr/DPR/dense_retriever.py \
- model_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/training_output/dpr_biencoder.26"\
- hydra.run.dir="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/inference/"\
+ model_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/training_output/dpr_biencoder.69"\
+ hydra.run.dir="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/inference_69/"\
  qa_dataset=strategy_gold_dev \
  ctx_datatsets=[dpr_strategyqa_tsv] \
- encoded_ctx_files=["/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/dense_embeddings/dpr_ctx_*"] \
- out_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/inference//retriever_result_gold.json"
+ encoded_ctx_files=["/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/dense_embeddings_69/dpr_ctx_*"] \
+ out_file="/home/joberant/home/urikatz1/dpr/experiments/training/gold_baseline_dpr_128_4g_ddp_nq/inference_69/retriever_result_gold.json"
